efa66bec6eec72338a7b669e3b965c7a2a2e1b63
neeraj31285 <neeraj.singh31285@gmail.com>
2026-02-26 23:34:24 +0530

    RTL integrated and Working.

--- ReflectSFMLApp/main.cpp
+++ ReflectSFMLApp/main.cpp
@@ -1,31 +1,20 @@
 
+#include <iostream>
+
 #include "cxx_mirror.h"
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/System/String.hpp>
-#include <SFML/Window/WindowBase.hpp>
-#include <SFML/System/FileInputStream.hpp>
 
 int main()
 {
-    sf::RenderWindow window(
-        sf::VideoMode(sf::Vector2u(800, 600)),
-        "SFML works"
-    );
+    auto getGlobalVolume = cxx::mirror().getFunction(cxx::fn::sf::Listener::getGlobalVolume::id)
+                                        ->argsT().returnT();
+    auto [err, gvol] = getGlobalVolume();
 
-    sf::SoundBuffer sndBuff("");
-
-    sf::Sound snd(sndBuff);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.display();
+    if (err != rtl::error::None) {
+        std::cout << "Reflection call error : " << rtl::to_string(err);
     }
+    else {
+        std::cout << "Reflection call successful, volume (returned) : " << gvol.view<float>()->get();
+    }
+
+    return 0;
 }
